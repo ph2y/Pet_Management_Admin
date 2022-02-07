fb1dcc3fda99f37a2842ecb03e577a63c90d0b26
newcentury99 <smyun99@gmail.com>
2022-02-07 13:01:48 +0900

    Init Project

---
+++ src/main/java/com/ph2y/pet_management_admin/PetManagementAdminApplication.java
@@ -0,0 +1,13 @@
+package com.ph2y.pet_management_admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetManagementAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetManagementAdminApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ph2y/pet_management_admin/PetManagementAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ph2y.pet_management_admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetManagementAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
